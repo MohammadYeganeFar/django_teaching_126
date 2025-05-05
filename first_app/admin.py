229e30173dde13f8a23ba68dbb59f9693e47fbee
Mohammad ubuntu <mmdvafaj@gmail.com>
2025-05-05 22:47:17 +0330

    add make_shown & hard_delete actions to admin.py

--- first_app/admin.py
+++ first_app/admin.py
@@ -1,11 +1,22 @@
 from django.contrib import admin
-
 from first_app.models import BlogPost, Comment, PostAuthor
+
+@admin.action(description="make post to shown")
+def make_shown(BlogPost, request, queryset):
+    queryset.update(is_shown=True)
+
+
+@admin.action(description="clear deleted objects from server")
+def hard_delete(BlogPost, request, queryset):
+    queryset.delete()
+
 
 @admin.register(BlogPost)
 class BlogPostAdmin(admin.ModelAdmin):
     list_display=('title', 'is_shown', 'created_at')
     list_filter=('is_shown','title')
+    actions = [make_shown]
+
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
